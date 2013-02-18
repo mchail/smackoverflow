8438f0c4147e3e54ba5f50526f73ad4503bdba65
Steve McHail <mchail@gmail.com>
2013-02-17 21:54:48 -0800

    added mongodb driver; added test mongo code; gitignore mongo data dir

--- app.js
+++ app.js
@@ -6,7 +6,37 @@
 var express = require('express')
   , routes = require('./routes')
   , http = require('http')
-  , path = require('path');
+  , path = require('path')
+  , MongoClient = require('mongodb').MongoClient
+  , format = require('util').format;
+
+
+
+var url = process.env.MONGOURL || "mongodb://localhost:27017/smackoverflow"
+
+MongoClient.connect(url, function(err, db) {
+  if(err) throw err;
+
+  console.log("connected");
+
+  var collection = db.collection('test');
+  var docs = [{mykey:1}, {mykey:2}, {mykey:3}];
+
+  collection.insert(docs, {w:1}, function(err, result) {
+    collection.find().toArray(function(err, items) {});
+
+      var stream = collection.find({mykey:{$ne:2}}).stream();
+
+      stream.on("data", function(item) {
+          console.log("MongoDB Item: ");
+          console.log(item);
+      });
+
+      stream.on("end", function() {});
+
+      collection.findOne({mykey:1}, function(err, item) {});
+  });  
+})
 
 var app = express();
 
